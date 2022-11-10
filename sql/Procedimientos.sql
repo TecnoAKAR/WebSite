c8d158e1297a72fb342b44d990b0abd501e44789
xXKalidLowënXx <98848611+KALID-AVAP@users.noreply.github.com>
2022-11-10 13:49:13 -0600

    Update Procedimientos.sql

--- sql/Procedimientos.sql
+++ sql/Procedimientos.sql
@@ -1,26 +1,27 @@
 use AKAR;
 drop procedure if exists registro;
 drop procedure if exists login;
+drop procedure if exists registromenordeedad;
 
 
 
 delimiter //
 
 create procedure registro( in nom nvarchar(50), pat nvarchar(50),
-mat nvarchar(50), fecha date, correo nvarchar(50),contraseña nvarchar (50), usuario nvarchar(50),tipo int)
+mat nvarchar(50), fecha date, correo nvarchar(50),contrasena nvarchar (50), usuario nvarchar(50),tipo int)
 begin
 declare xIdPersona int;
 declare xIdTipo int;
 	
     insert into Usuario( Nombre, ApellidoP, ApellidoM, Correo, Contrasena, NomUsuario, Fecha)
-    values (nom, pat, mat, correo, contraseña, usuario, fecha);
+    values (nom, pat, mat, correo, contrasena, usuario, fecha);
     set xIdPersona=( select idUsuario from Usuario where Nombre= nom and ApellidoP= pat);
     set xIdTipo= tipo;
     insert into RelTipoUsuario( idUsuario, idTipo) values (xIdPersona,xIdTipo);
 
 end; //
 
-create procedure login(correo nvarchar(50),contraseña nvarchar(50))
+create procedure login(correo nvarchar(50),contrasena nvarchar(50))
 begin
 declare xidPersona int;
 declare existe int;
@@ -32,4 +33,26 @@ declare xMsj nvarchar(50);
 			set xidPersona=(select idUsuario from Usuario where Correo=correo and Contrasena=contrasena);
 			select * from Usuario where idusuario=xidPersona;
 		end if;
+end; //
+
+create procedure registromenordeedad ( in nom nvarchar(50), pat nvarchar(50),
+mat nvarchar(50), fecha date, correo nvarchar(50),contrasena nvarchar (50), usuario nvarchar(50),nom2 nvarchar(50), pat2 nvarchar(50),
+mat2 nvarchar(50), fecha2 date, correo2 nvarchar(50),contrasena2 nvarchar (50), usuario2 nvarchar(50))
+begin
+declare xIdPersona int;
+declare xIdPersona2 int;
+declare xIdTipo int;
+declare xIdTipo2 int;
+
+	
+    insert into Usuario( Nombre, ApellidoP, ApellidoM, Correo, Contrasena, NomUsuario, Fecha)
+    values (nom, pat, mat, correo, contrasena, usuario, fecha),(nom2, pat2, mat2, correo2, contrasena2, usuario2, fecha2);
+    set xIdPersona=( select idUsuario from Usuario where Nombre= nom and ApellidoP= pat);
+    set xIdPersona2=( select idUsuario from Usuario where Nombre= nom2 and ApellidoP= pat2);
+    set xIdTipo= 2;
+	set xIdTipo= 3;
+    insert into RelTipoUsuario( idUsuario, idTipo) values (xIdPersona,xIdTipo),(xIdPersona2,xIdTipo2);
+	insert into RelTutorUCSA ( idUsuario1, idUsuario2) values( xIdPersona,xIdPersona2);
+    
+    
 end; //
