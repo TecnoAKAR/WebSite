6cdc51e59bcb4f2b5eb63af6ca81194ec0fd7d2c
xXKalidLowënXx <98848611+KALID-AVAP@users.noreply.github.com>
2022-11-10 08:17:30 -0600

    Update Procedimientos.sql

--- sql/Procedimientos.sql
+++ sql/Procedimientos.sql
@@ -1,5 +1,9 @@
 use AKAR;
 drop procedure if exists registro;
+drop procedure if exists login;
+
+
+
 delimiter //
 
 create procedure registro( in nom nvarchar(50), pat nvarchar(50),
@@ -14,4 +18,18 @@ declare xIdTipo int;
     set xIdTipo= tipo;
     insert into RelTipoUsuario( idUsuario, idTipo) values (xIdPersona,xIdTipo);
 
+end; //
+
+create procedure login(correo nvarchar(50),contraseña nvarchar(50))
+begin
+declare xidPersona int;
+declare existe int;
+declare xMsj nvarchar(50);
+		set existe=(select count(*) from Usuario where Correo=correo and Contrasena=contrasena);
+        if(existe=0) then
+			set xMsj="No existe usuario";
+        elseif(existe=1)then
+			set xidPersona=(select idUsuario from Usuario where Correo=correo and Contrasena=contrasena);
+			select * from Usuario where idusuario=xidPersona;
+		end if;
 end; //
