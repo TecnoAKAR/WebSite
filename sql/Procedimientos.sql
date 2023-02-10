a09120d531ec4224b1f1eb83dccb7a2bf898c36e
unknown <zarate.lozano.luis.axel@gmail.com>
2023-02-09 22:11:21 -0600

    DB debug

--- sql/Procedimientos.sql
+++ sql/Procedimientos.sql
@@ -15,7 +15,7 @@ declare xIdPersona int;
 declare xIdTipo int;
 declare existe int;
 declare xMsj nvarchar(50);
-	set existe=(select count(*) from usuario where Correo=correo and Nombre=nom);
+	set existe=(select count(*) from usuario where Usuario.Correo=correo or Usuario.NomUsuario = usuario);
     if (existe=0) then
 		insert into Usuario( Nombre, ApellidoP, ApellidoM, Correo, Contrasena, NomUsuario, Fecha)
 		values (nom, pat, mat, correo, sha(contrasena), usuario, fecha);
@@ -27,7 +27,7 @@ declare xMsj nvarchar(50);
     Usuario.ApellidoM, Usuario.Correo, Usuario.NomUsuario  from RelTipoUsuario inner join Usuario  on RelTipoUsuario.idUsuario = Usuario.idUsuario inner join TipoUsuario on RelTipoUsuario.idTipo = TipoUsuario.idTipo where RelTipoUsuario.idUsuario = xidPersona;
 	else
 		if(existe>0)then
-		set xMsj="Usuario Registrado";
+		set xMsj="Usuario o correo en uso";
         select xMsj;
 end if;
 end if;
@@ -52,27 +52,33 @@ set xContraSha =(select sha(contrasenaP));
 end; //
 
 create procedure sp_Registromenordeedad ( in nom nvarchar(50), pat nvarchar(50),
-mat nvarchar(50), fecha date, correo nvarchar(50),contrasena nvarchar (50), usuario nvarchar(50),correo2 nvarchar(50),usuario2 nvarchar(50))
+mat nvarchar(50), fecha date, correo nvarchar(50),contrasena nvarchar (50), usuario nvarchar(50), tipo int, correo2 nvarchar(50))
 begin
 declare xIdPersona int;
 declare xIdPersona2 int;
 declare xIdTipo int;
 declare existe int;
-declare menor int;
+declare menorexist int;
+declare tutorexist int;
 declare xMsj nvarchar(50);
-    set menor= (select count(*) from Usuario where Correo=correo and Nombre=nom);
-			if(menor=0)then
+    set tutorexist = (select count(*) from Usuario where Usuario.Correo=correo2);
+    set menorexist = (select count(*) from Usuario where Usuario.Correo=correo or Usuario.NomUsuario = usuario);
+			if(tutorexist>0 and menorexist=0)then
 			insert into Usuario( Nombre, ApellidoP, ApellidoM, Correo, Contrasena, NomUsuario, Fecha)
 			values (nom, pat, mat, correo, sha(contrasena), usuario, fecha);
-			set xIdPersona=( select idUsuario from Usuario where Nombre= nom and ApellidoP= pat);
-			set xIdPersona2=( select idUsuario from Usuario where Correo= correo2 and NomUsuario= usuario2);
-			set xIdTipo= 2;
+			set xIdPersona=( select idUsuario from Usuario where NomUsuario = usuario and Usuario.Correo = correo);
+			set xIdPersona2=( select idUsuario from Usuario where Usuario.Correo = correo2);
+			set xIdTipo= tipo;
 			insert into RelTipoUsuario( idUsuario, idTipo) values (xIdPersona,xIdTipo);
 			insert into RelTutorUCSA ( idUsuario1, idUsuario2) values( xIdPersona,xIdPersona2);
 			select RelTipoUsuario.idRelTipoUsuario, RelTipoUsuario.idUsuario, RelTipoUsuario.idTipo, Usuario.Nombre,Usuario.ApellidoP, Usuario.ApellidoM, Usuario.Correo, Usuario.NomUsuario from RelTipoUsuario inner join Usuario  on RelTipoUsuario.idUsuario = Usuario.idUsuario inner join TipoUsuario on RelTipoUsuario.idTipo = TipoUsuario.idTipo where RelTipoUsuario.idUsuario = xidPersona;
 			else
-				if(menor>0)then
-					set xMsj= "Menor ya registrado";
+				if(tutorexist=0)then
+					set xMsj= "El tutor no existe";
+					select xMsj;
+                    end if;
+				if(menorexist=1)then
+					set xMsj= "El usuario ya existe";
 					select xMsj;
 end if;
 end if;  
