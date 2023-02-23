9486311578da6496e0d5a56166f170bf3e18f977
Lu1sGH <99305390+Lu1sGH@users.noreply.github.com>
2023-02-23 08:46:52 -0600

    Update Correo.java

--- src/java/org/akar/extConnections/Correo.java
+++ src/java/org/akar/extConnections/Correo.java
@@ -10,17 +10,17 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class Correo {
-    private String akarSupport;
-    private String clave;
-    private String correoAKAR;
+    private final String akarSupport;
+    private final String clave;
+    private final String correoAKAR;
     
     public Correo(){
-    }
-    
-    public boolean contactoAKAR(String nombre, String mail, String asunto, String cuerpo) {
         this.akarSupport = "tecno.akar.soporte@gmail.com";
         this.clave = "fgkebykjmmafajmu";
         this.correoAKAR = "akartecnologias@gmail.com";
+    }
+    
+    public boolean contactoAKAR(String nombre, String mail, String asunto, String cuerpo) {
 
         Properties props = System.getProperties();
         props.put("mail.smtp.host", "smtp.gmail.com");
@@ -52,8 +52,34 @@ public class Correo {
         
     }
     
-    public boolean resPass(){
-        return false;
+    public boolean resPass(String user, String mail, String token){
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.user", this.akarSupport);
+        props.put("mail.smtp.clave", this.clave);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        String msj = "Hola, "+user+".\nPara poder restablecer su contraseña ingresa en el siguiente link: \n"+"http://localhost:8080/akar/resPass.jsp?token="+token+"\nPodrá cambiar su contraseña en un plazo máximo de 24 horas.\nCualquier situación que se le presente contactenos a "+this.correoAKAR+", estamos para servirle";
+        
+        try {
+            message.setFrom(new InternetAddress(akarSupport));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(mail));
+            message.setSubject("Correo para restablecer contraseña.");
+            message.setText(msj);
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", akarSupport, clave);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+            return true;
+        } catch (MessagingException me) {
+            me.printStackTrace();
+            return false;
+        }
     }
     
 }
