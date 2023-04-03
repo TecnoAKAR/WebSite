20773a01faa64add3e4f0b93a864b7296eb2440b
unknown <zarate.lozano.luis.axel@gmail.com>
2023-04-02 23:53:15 -0500

    IngSoporte

--- src/java/org/akar/helper/SolucionHelper.java
+++ src/java/org/akar/helper/SolucionHelper.java
@@ -1,76 +1,107 @@
 
 package org.akar.helper;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import org.akar.dao.RelReporteUsuario;
 import org.akar.dao.ReporteCambios;
 import org.akar.dao.Reporte;
+import org.akar.dao.TblUsuario;
 import org.akar.service.SolucionService;
 
 public class SolucionHelper {
     
     Reporte rep;
     ReporteCambios repCam;
+    TblUsuario usu;
     
     public SolucionHelper(){
         rep = new Reporte();
-        rel = new ReporteCambios();
+        repCam = new ReporteCambios();
+        usu = new TblUsuario();
     }
-//    
-//    public List<Reporte> getListSop(){
-//        List<Reporte> list = new SolucionService().getListSop();
-//        if(list != null){
-//            return list;
-//        } else{
-//            return null;
-//        }
-//    }
-//    
-//    public List<Reporte> getListMan(){
-//        List<Reporte> list = new SolucionService().getListMan();
-//        if(list != null){
-//            return list;
-//        } else{
-//            return null;
-//        }
-//    }
-//    
-//    public Reporte getById(HttpServletRequest par){
-//        sol.setIdTarea( Integer.parseInt(par.getParameter("idreporte")) );
-//        
-//        if(sol.getIdTarea() == 0){
-//            return null;
-//        }
-//        
-//        Reporte rep = new SolucionService().getById(sol);
-//        if(rep != null){
-//            return rep;
-//        } else{
-//            return null;
-//        }
-//    }
-//    
-//    public boolean update(HttpServletRequest par){
-//        sol.setIdTarea( Integer.parseInt(par.getParameter("idTarea")) );
-//        sol.setEstatus( par.getParameter("estatus") );
-//        sol.setSolucion( par.getParameter("sol") );
-//        
-//        if(sol.getSolucion().length() > 520 || sol.getSolucion().length() == 0 || sol.getSolucion() == null || sol.getSolucion().equals("")){
-//            return false;
-//        }
-//        if(sol.getEstatus().equals(par.getParameter("estatusInicial"))){
-//            return false;
-//        }
-//        if(sol.getIdTarea() == 0){
-//            return false;
-//        }
-//        
-//        if(new SolucionService().update(sol) == true){
-//            return true;
-//        } else{
-//            return false;
-//        }
-//        
-//    }
-//    
+    
+    public List<Reporte> getListSop(int idUsuario){
+        usu.setIdUsuario(idUsuario);
+
+        if(usu.getIdUsuario() == 0){
+            return null;
+        }
+        
+        List<Reporte> list = new SolucionService().getListSop(usu);
+        if(list != null){
+            return list;
+        } else{
+            return null;
+        }
+    }
+    
+    public List<Reporte> getListMan(int idUsuario){
+        usu.setIdUsuario(idUsuario);
+        if(usu.getIdUsuario() == 0){
+            return null;
+        }
+        
+        List<Reporte> list = new SolucionService().getListMan(usu);
+        if(list != null){
+            return list;
+        } else{
+            return null;
+        }
+    }
+    
+    public RelReporteUsuario getById(HttpServletRequest par){
+        rep.setIdReporte( Integer.parseInt(par.getParameter("idreporte")) );
+        
+        if(rep.getIdReporte() == 0){
+            return null;
+        }
+        
+        RelReporteUsuario report = new SolucionService().getById(rep);
+        if(report != null){
+            return report;
+        } else{
+            return null;
+        }
+    }
+    
+    public boolean sp_Soporte(HttpServletRequest par){
+        Date fecha = new Date();
+        rep.setIdReporte( Integer.parseInt(par.getParameter("idRep")) );
+        rep.setEstatus( par.getParameter("estat") );
+        rep.setSolucion( par.getParameter("sol") );
+        rep.setFechaF( fecha );
+        repCam.setEstatusI( par.getParameter("estatI") );
+        repCam.setFechaCambio( fecha );
+        usu.setIdUsuario( Integer.parseInt(par.getParameter("idIng")) );
+        
+        if(repCam.getEstatusI() == null ||  repCam.getEstatusI().length() == 0){
+            return false;
+        }
+        if(repCam.getFechaCambio() == null ){
+            return false;
+        }
+        if(usu.getIdUsuario() == 0 ){
+            return false;
+        }
+        if(rep.getIdReporte()== 0){
+            return false;
+        }
+        if(rep.getSolucion() == null || rep.getSolucion().length() > 1024 || rep.getSolucion().length() == 0 || rep.getSolucion().equals("")){
+            return false;
+        }
+        if(rep.getEstatus().equals( repCam.getEstatusI() )){
+            return false;
+        }
+        if(rep.getFechaF() == null ){
+            return false;
+        }
+        
+        return new SolucionService().sp_Soporte(rep, usu, repCam);
+    }
+    
 }
