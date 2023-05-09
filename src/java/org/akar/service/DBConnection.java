9dc0768cad4f38b9dde43fb680cc556ba5360c90
Lu1sGH <99305390+Lu1sGH@users.noreply.github.com>
2023-05-09 08:29:08 -0600

    New DBConnection

--- src/java/org/akar/service/DBConnection.java
+++ src/java/org/akar/service/DBConnection.java
@@ -1,77 +1,38 @@
 package org.akar.service;
 
-import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection implements Serializable{
-    public static String user = "root";
-    public static String password = "n0m3l0";
-    public static String db = "AKAR";
-    
-    public static boolean testDriver(){
-        try 
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            return true;
-        } 
-        catch (Exception ex) 
-        {
+public class DBConnection {
+    private static final String url = "jdbc:mysql://localhost:3308/AKAR";
+    private static final String user = "root";
+    private static final String password = "n0m3l0";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error al cargar el controlador JDBC");
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            System.out.println("Error al establecer la conexión a la base de datos");
             ex.printStackTrace();
         }
-        return false;
+        return connection;
     }
-    
-    public static Connection getConnection(String user, String password , String db , String server){
-        String url = null;
-        if( user == null || password == null || db == null || server == null )
-        {
-            return null;
-        }
-        if( "".equals(user) || "".equals(password) || "".equals(db) || "".equals(server) )
-        {
-            return null;
-        }
-        url = String.format( "jdbc:mysql://%s/%s?user=%s&password=%s" , server , db , user , password );
-        try 
-        {
-            if( !testDriver() )
-            {
-                return null;
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println("Error al cerrar la conexión a la base de datos");
+                ex.printStackTrace();
             }
-            return (Connection) DriverManager.getConnection(url);
-        } 
-        catch (SQLException ex) 
-        {
-            ex.printStackTrace();
-        }
-        return null;
-    }
-    
-    public static java.sql.Connection getConnection( )
-    {
-        return (java.sql.Connection) getConnection( user, password, db , "127.0.0.1" );
-    }
-    
-    public static void closeConnection( java.sql.Connection connection )
-    {
-        try 
-        {
-            if( connection == null )
-            {
-                return;
-            }
-            if(connection.isClosed() )
-            {
-                return;
-            }
-            connection.close();
-        } 
-        catch (SQLException ex) 
-        {
-            ex.printStackTrace();
         }
     }
-    
 }
+
